a5132d705a714aae8912fb8965fd8f71fc958b3d
Apoorv0503 <apdwi.ad@gmail.com>
2024-09-21 10:33:06 +0530

    added the handle search logic && extracted API logic

--- src/App.js
+++ src/App.js
@@ -11,9 +11,32 @@ function App() {
   const [totalPages, setTotalPages] = useState(10);
   const [books, setBooks] = useState([]);
 
-  const handleSearch=()=>{
-    // 
+  const handleSearch = async () => {
+  //if query is empty
+  if (!query.trim()) return; 
+
+  try {
+    // made API call to OpenLibrary's search 
+    const response = await axios.get(`https://openlibrary.org/search.json?q=${query}`);
+    // &page=${totalPages}
+    
+    console.log(response.data);
+    // extract the books data
+    const booksData = response.data.docs.map(book => ({
+      key: book.key,
+      title: book.title,
+      author_name: book.author_name?.[0], // Handle cases where author name is not available
+      cover_id: book.cover_i, // Use cover_i for the cover image
+    }));
+
+    setBooks(booksData);
+    setTotalPages(Math.ceil(response.data.numFound / 20)); // calculate total pages (20 results per page)
+
+  } catch (error) {
+    console.error("Error fetching books:", error);
   }
+};
+
   return (
     <div className="app">
         <SearchBar query={query} setQuery={setQuery} handleSearch={handleSearch} />
